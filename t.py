d8800fcd7e6f540d78374355de5fdb9fd4e9cc35
Sean Lee <freshdried@gmail.com>
2013-11-27 17:10:18 -0500

    moved to tabdown

--- t.py
+++ t.py
@@ -1,70 +1,47 @@
-#!/usr/bin/env python
-
-import os
-import sys
+import tabdown
 import re
-import json
-from collections import OrderedDict
 
-#idea: let hashtag mean when!
-#tab-delimited ordered tree todolist
-#idea: general tab delimited outliner!
-#idea: tabdown!
 
-def parse(lines):
+@tabdown.on_parse
+def todo_parse(line):
+	"""Temporal significance is given to lines starting with a hashtag
+	Takes a list of lines, returns a tree
+	"""
+	node = {}
+	splitted = re.compile('(^#+)').split(line)
+
+	if len(splitted) > 1:
+		node["type"] = "when"
+		node["text"] = splitted[2]
+	else:
+		node["type"] = "what"
+		node["text"] = splitted[0]
+
+	return node
+
+def get_tasks(tree):
+    """Takes tree, returns list of tasks
+    task = {"what": [], "when": []}
     """
-    Returns a tree (OrderedDict of OrderedDicts) from a tab-marked list of lines.
-    Goes through lines line-by-line.
-    """
-    tree = OrderedDict()
-    levels = [tree]
-    for line in lines:
-        tabs = lambda line: len(line) - len(line.lstrip('\t'))
+    tasks = []
+    def _list(node, what, when):
+        if node["type"] == "what":
+            what.append(node["text"])
 
-        top = levels[-1]
+        elif node["type"] == "when":
+            when.append(node["text"])
 
-        tabdiff = tabs(line) - len(levels) + 1
-        if tabdiff > 0:
-            k,v = top.popitem() #pop off last item in top
-            top[k] = v #readd it
 
-            levels.append(v) #make it the new highest level
-            top = levels[-1] #top = newtop
-
-        if tabdiff < 0:
-            for i in range(-1 * tabdiff):
-                oldtop = levels.pop()
-            top = levels[-1]
-
-        top[line.lstrip('\t')] = OrderedDict()
-    return tree
-
-def reprint(tree):
-    """
-    Prints a tree (OrderedDict of OrderedDicts) marked by tabs 
-    Depth-first, recursive traversal of tree
-    """
-    def _testprint(tree, level):
-        if len(tree) == 0:
+        if not "children" in node.keys():
+            tasks.append({"what": what, "when": when})
             return
-        for k,v in tree.items():
-            print("\t"*level + k)
-            _testprint(v, level + 1)
-    _testprint(tree, 0)
 
-def main():
-    path = "s"
-    file = open(os.environ['HOME'] + '/' + path)
+        for child in node['children']:
+            _when = when.copy()
+            _what = what.copy()
+            _list(child, _what, _when)
 
-    lines = list(filter(lambda x: not x == '', file.read().split('\n'))) #ignore blank lines
+    for child in tree["children"]:
+        _list(child, [], [])
+    return tasks
 
-    tree = parse(lines)
-
-
-    print(json.dumps(tree,indent=4))
-    reprint(tree)
-
-if len(sys.argv) == 2:
-    None
-else:
-    main()
