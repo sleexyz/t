00f88a58d3e2b644b0bbf05ca5dfd00a4279eff5
Sean Lee <freshdried@gmail.com>
2014-03-17 19:34:56 -0400

    implement argparse stuff

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ __t__omorrow.
 <br>
 <br>
 
-t is a program built off of [tabdown](https://github.com/freshdried/tabdown.git) to organize mental clutter in regards to today and tomorrow.<br>It includes a calendar system, a todo list system, and a basic list system.
+t is text-based organization system using [tabdown](https://github.com/freshdried/tabdown.git) to reduce mental clutter in regards to today and tomorrow.<br>It includes a calendar system, a todo list system, and a basic list system.
 
 <br>
 <br>
@@ -23,8 +23,5 @@ t is a program built off of [tabdown](https://github.com/freshdried/tabdown.git)
 - get calendar formatted with columns
 - get todo formatted
 - get list formatted
-- get argv working
 - make it use homedirectory/s (and then maybe a config file)
-- browser?
 - add to npm with install -g stuff
-
--- bin/t.js
+++ bin/t.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 var fs = require('fs'),
+    colors = require('colors'),
     T = require('../');
 
 var path = '/home/slee2/s'
@@ -11,13 +12,62 @@ var plugins = {
 	"list": new (require('../lib/list')),
 }
 
+var otherfuncs = (function() {
+	function help() {
+		var message = "Usage: t [module] [function]\n\nModules:";
+		console.log(message);
+		for (key in plugins) {
+			console.log(' ' + key + '\t' + plugins[key].about);
+		}
+		console.log("\nOther functions:\n help\tshow this info\n about\tshow about info");
+		console.log("\ntype `t [module] help` for module info and usage");
+	}
+	function about() {
+		var message = "\n\t" + " t".yellow + "\n\n" + " t".yellow + "ab structured\n" + " t".yellow + "rees to plan for\n"+ " t".yellow + "oday and\n" + " t".yellow + "omorrow.\n\n\n" + "t".yellow + " is a program built off of tabdown to organize\nmental clutter in regards to today and tomorrow.\nIt includes a calendar system, a todo list system,\nand a basic list system.\n\n\nCreated by Sean Lee\n";
+		console.log(message);
+	}
+
+	return {
+		"help": help,
+		"--help": help,
+    		"about": about,
+	}
+})();
+var noargsfunction = otherfuncs.help;
+
+function parseargs(argv) {
+	if (argv.length == 0) {
+		noargsfunction();
+	} else if (argv.length == 1){
+		var arg = argv[0];
+		if (plugins[arg] !== undefined) {
+			plugins[arg].defaultfunction();
+		} else if (otherfuncs[arg] !== undefined) {
+			otherfuncs[arg]();
+		}else {
+			console.error('Error parsing options...');
+			otherfuncs.help();
+		}
+	} else {
+		if (plugins[argv[0]] !== undefined) {
+			var func = plugins[argv[0]].functions[argv[1]];
+			if (func !== undefined) {
+				func(argv.splice(2));
+			} else {
+				console.error('Error parsing options for ' + argv[0] + '...');
+				console.log(plugins[argv[0]].help);
+			}
+		} else {
+			console.error('Error parsing options...');
+			otherfuncs.help();
+		}
+	}
+}
+
 fs.readFile(path, function(err, data) {
 	var lines = data.toString().split('\n');
 	var t = new T(lines, plugins);
-	argv[]
 
-	//plugins.todo.functions.print();
-	plugins.cal.functions.print();
-	//plugins.list.functions.print();
-	//do some sort of parseargs thing, and have subcommands like git does
+	var argv = process.argv.splice(2);
+	parseargs(argv);
 });
